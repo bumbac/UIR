52d1a477f249c0276a23c1991f94bd205c01ffea
sutymate <sutymate@fit.cvut.cz>
2022-10-05 09:28:34 +0200

    init

---
+++ hexapod_explorer/HexapodExplorer.py
@@ -0,0 +1,234 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import copy
+
+# import messages
+from messages import *
+
+
+# cpg network
+# import cpg.oscilator_network as osc
+
+class HexapodExplorer:
+
+    def __init__(self):
+        pass
+
+    def update_occupied(self, Pmi):
+        """method to calculate the Bayesian update of the occupied cell with the current occupancy probability value P_mi
+        Args:
+            P_mi: float64 - current probability of the cell being occupied
+        Returns:
+            p_mi: float64 - updated probability of the cell being occupied
+        """
+        pz_occ = (1 + .95 - 0) / 2
+        pz_free = 1 - pz_occ
+        Pmi_free = 1 - Pmi
+        Pmi = (pz_occ*Pmi) / (pz_occ*Pmi + pz_free*Pmi_free)
+        if Pmi == 1:
+            Pmi = 0.9
+        return Pmi
+
+    def update_free(self, Pmi):
+        """method to calculate the Bayesian update of the free cell with the current occupancy probability value P_mi
+        Args:
+            P_mi: float64 - current probability of the cell being occupied
+        Returns:
+            p_mi: float64 - updated probability of the cell being occupied
+        """
+        Pmi_occ = self.update_occupied(Pmi)
+        return 1 - Pmi_occ
+
+    def bresenham_line(self, start, goal):
+        """Bresenham's line algorithm
+        Args:
+            start: (float64, float64) - start coordinate
+            goal: (float64, float64) - goal coordinate
+        Returns:
+            interlying points between the start and goal coordinate
+        """
+        (x0, y0) = start
+        (x1, y1) = goal
+        line = []
+        dx = abs(x1 - x0)
+        dy = abs(y1 - y0)
+        x, y = x0, y0
+        sx = -1 if x0 > x1 else 1
+        sy = -1 if y0 > y1 else 1
+        if dx > dy:
+            err = dx / 2.0
+            while x != x1:
+                line.append((x, y))
+                err -= dy
+                if err < 0:
+                    y += sy
+                    err += dx
+                x += sx
+        else:
+            err = dy / 2.0
+            while y != y1:
+                line.append((x, y))
+                err -= dx
+                if err < 0:
+                    x += sx
+                    err += dy
+                y += sy
+        x = goal[0]
+        y = goal[1]
+        return line
+
+    def position_to_coordinates(self, position, origin, resolution):
+        x = np.floor((position.x - origin.x) / resolution)
+        y = np.floor((position.y - origin.y) / resolution)
+        z = np.floor((position.z - origin.y) / resolution)
+        return int(x), int(y), int(z)
+
+
+    def fuse_laser_scan(self, grid_map, laser_scan, odometry):
+        """ Method to fuse the laser scan data sampled by the robot with a given 
+            odometry into the probabilistic occupancy grid map
+        Args:
+            grid_map: OccupancyGrid - gridmap to fuse te laser scan to
+            laser_scan: LaserScan - laser scan perceived by the robot
+            odometry: Odometry - perceived odometry of the robot
+        Returns:
+            grid_map_update: OccupancyGrid - gridmap updated with the laser scan data
+        """
+        grid_map_update_object = copy.deepcopy(grid_map)
+        print(grid_map_update_object)
+        print(odometry)
+        print(laser_scan)
+        if (grid_map is not None) and (odometry is not None) and (laser_scan is not None):
+            grid_map_update = grid_map.data.reshape(grid_map_update_object.height, grid_map_update_object.width)
+
+            origin = grid_map.origin.position
+            resolution = grid_map.resolution
+            robot_position = odometry.pose.position
+            robot_coordinates = self.position_to_coordinates(robot_position, origin, resolution)
+            rotation_matrix = odometry.pose.orientation.to_R()
+            print(rotation_matrix)
+            free_line_coordinates = copy.deepcopy(laser_scan.distances)
+
+            for i, beam in enumerate(laser_scan.distances):
+                angle = laser_scan.angle_min + i*laser_scan.angle_increment
+                x = beam * np.cos(angle)
+                y = beam * np.sin(angle)
+                z = 0
+                point = rotation_matrix @ [x, y, z] + [robot_position.x, robot_position.y, z]
+                position = Vector3(point[0], point[1], point[2])
+                coordinates = self.position_to_coordinates(position, origin, resolution)
+                free_line_coordinates[i] = self.bresenham_line((robot_coordinates[0], robot_coordinates[1]),
+                                                               (coordinates[0], coordinates[1]))
+                grid_map_update[coordinates[0]][coordinates[1]] = self.update_occupied(grid_map_update[coordinates[0]][coordinates[1]])
+                for _, free_coord in enumerate(free_line_coordinates[i]):
+                    x, y = free_coord
+                    grid_map_update[x][y] = self.update_free(grid_map_update[x][y])
+            grid_map_update_object.data = grid_map_update.flatten()
+        return grid_map_update_object
+
+
+    def find_free_edge_frontiers(self, grid_map):
+        """Method to find the free-edge frontiers (edge clusters between the free and unknown areas)
+        Args:
+            grid_map: OccupancyGrid - gridmap of the environment
+        Returns:
+            pose_list: Pose[] - list of selected frontiers
+        """
+
+        #TODO:[t1e_expl] find free-adges and cluster the frontiers
+        return None 
+
+
+    def find_inf_frontiers(self, grid_map):
+        """Method to find the frontiers based on information theory approach
+        Args:
+            grid_map: OccupancyGrid - gridmap of the environment
+        Returns:
+            pose_list: Pose[] - list of selected frontiers
+        """
+
+        #TODO:[t1e_expl] find the information rich points in the environment
+        return None
+
+
+    def grow_obstacles(self, grid_map, robot_size):
+        """ Method to grow the obstacles to take into account the robot embodiment
+        Args:
+            grid_map: OccupancyGrid - gridmap for obstacle growing
+            robot_size: float - size of the robot
+        Returns:
+            grid_map_grow: OccupancyGrid - gridmap with considered robot body embodiment
+        """
+
+        grid_map_grow = copy.deepcopy(grid_map)
+
+        #TODO:[t1d-plan] grow the obstacles for robot_size
+
+        return grid_map_grow
+
+
+    def plan_path(self, grid_map, start, goal):
+        """ Method to plan the path from start to the goal pose on the grid
+        Args:
+            grid_map: OccupancyGrid - gridmap for obstacle growing
+            start: Pose - robot start pose
+            goal: Pose - robot goal pose
+        Returns:
+            path: Path - path between the start and goal Pose on the map
+        """
+
+        path = Path()
+        #add the start pose
+        path.poses.append(start)
+        
+        #TODO:[t1d-plan] plan the path between the start and the goal Pose
+        
+        #add the goal pose
+        path.poses.append(goal)
+
+        return path
+
+    def simplify_path(self, grid_map, path):
+        """ Method to simplify the found path on the grid
+        Args:
+            grid_map: OccupancyGrid - gridmap for obstacle growing
+            path: Path - path to be simplified
+        Returns:
+            path_simple: Path - simplified path
+        """
+        if grid_map == None or path == None:
+            return None
+ 
+        path_simplified = Path()
+        #add the start pose
+        path_simplified.poses.append(path.poses[0])
+        
+        #TODO:[t1d-plan] simplifie the planned path
+        
+        #add the goal pose
+        path_simplified.poses.append(path.poses[-1])
+
+        return path_simplified
+ 
+    ###########################################################################
+    #INCREMENTAL Planner
+    ###########################################################################
+
+    def plan_path_incremental(self, grid_map, start, goal):
+        """ Method to plan the path from start to the goal pose on the grid
+        Args:
+            grid_map: OccupancyGrid - gridmap for obstacle growing
+            start: Pose - robot start pose
+            goal: Pose - robot goal pose
+        Returns:
+            path: Path - path between the start and goal Pose on the map
+        """
+
+        if not hasattr(self, 'rhs'): #first run of the function
+            self.rhs = np.full((grid_map.height, grid_map.width), np.inf)
+            self.g = np.full((grid_map.height, grid_map.width), np.inf)
+
+        #TODO:[t1x-dstar] plan the incremental path between the start and the goal Pose
+ 
+        return self.plan_path(grid_map, start, goal), self.rhs.flatten(), self.g.flatten()
---
+++ hexapod_robot/HexapodController.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import math
+
+import numpy as np
+from messages import *
+
+DELTA_DISTANCE = 0.12
+C_TURNING_SPEED = 5
+C_AVOID_SPEED = 10
+
+
+class HexapodController:
+    def __init__(self):
+        pass
+
+    def goto(self, goal, odometry, collision):
+        """Method to steer the robot towards the goal position given its current 
+           odometry and collision status
+        Args:
+            goal: Pose of the robot goal
+            odometry: Perceived odometry of the robot
+            collision: bool of the robot collision status
+        Returns:
+            cmd: Twist steering command
+        """
+        # zero velocity steering command
+        cmd_msg = Twist()
+        if (goal is not None) and (odometry is not None) and (collision is not None):
+
+            if collision:
+                return None
+
+            diff = goal.position - odometry.pose.position
+            dist_to_goal = diff.norm()
+            # print(dist_to_goal)
+
+            if dist_to_goal < DELTA_DISTANCE:
+                return None
+            goal_h = np.arctan2(diff.y, diff.x)
+            # print(goal_h)
+
+            robot_h = odometry.pose.orientation.to_Euler()[0]
+            # print(robot_h)
+
+            diff_h = goal_h - robot_h
+            # magic
+            diff_h = (diff_h + math.pi) % (2*math.pi) - math.pi
+
+            cmd_msg.linear.x = dist_to_goal
+            cmd_msg.angular.z = C_TURNING_SPEED*diff_h
+
+        return cmd_msg
+
+    def goto_reactive(self, goal, odometry, collision, laser_scan):
+        """Method to steer the robot towards the goal position while avoiding 
+           contact with the obstacles given its current odometry, collision 
+           status and laser scan data
+        Args:
+            goal: Pose of the robot goal
+            odometry: Perceived odometry of the robot
+            collision: bool of the robot collision status
+            laser_scan: LaserScan data perceived by the robot
+        Returns:
+            cmd: Twist steering command
+        """
+        cmd_msg = Twist()
+        if (goal is not None) and (odometry is not None) and (collision is not None) and (laser_scan is not None):
+            cmd_msg = self.goto(goal=goal, odometry=odometry, collision=collision)
+            if cmd_msg is None:
+                return cmd_msg
+            diff = goal.position - odometry.pose.position
+            linear_speed = diff.norm()
+            if linear_speed < DELTA_DISTANCE:
+                return None
+            goal_h = np.arctan2(diff.y, diff.x)
+            robot_h = odometry.pose.orientation.to_Euler()[0]
+            dphi = goal_h - robot_h
+            dphi = (dphi + math.pi) % (2 * math.pi) - math.pi
+
+
+            l = len(laser_scan.distances)
+            left_points = laser_scan.distances[:l//2]
+            right_points = laser_scan.distances[l//2:]
+
+            # scan_left = min(left_points)
+            # scan_right = min(right_points)
+            # repulsive_force = 0
+            # if scan_right > 0 and scan_left > 0:
+            #     repulsive_force = 1/scan_left - 1/scan_right
+
+            scan_left = [point for i, point in enumerate(left_points) if laser_scan.range_min < point < laser_scan.range_max]
+            scan_right = [point for i, point in enumerate(right_points) if laser_scan.range_min < point < laser_scan.range_max]
+            repulsive_force = 1/min(scan_left) - 1/min(scan_right)
+
+            angular_speed_navigation_component = dphi*C_TURNING_SPEED
+            angular_speed_avoidance_component = repulsive_force*C_AVOID_SPEED
+            angular_speed = angular_speed_avoidance_component + angular_speed_navigation_component
+
+            # cmd_msg.linear.x = linear_speed
+            cmd_msg.angular.z = C_TURNING_SPEED * angular_speed
+
+        return cmd_msg
